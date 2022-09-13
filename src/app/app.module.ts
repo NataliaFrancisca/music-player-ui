95074d6a417c95f079c1bbcde706087c27a4d608
NataliaFrancisca <nathifrancisca@gmail.com>
2022-09-13 16:07:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { NavContentComponent } from './components/nav-content/nav-content.component';
+import { MenuContentComponent } from './components/menu-content/menu-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    NavContentComponent,
+    MenuContentComponent
   ],
   imports: [
     BrowserModule,
