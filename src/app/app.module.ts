04776998a59d33fab802ec5891c862ec7a1a622e
NataliaFrancisca <nathifrancisca@gmail.com>
2022-09-14 11:43:08 -0300

    feat: delete component was not used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { NavContentComponent } from './components/nav-content/nav-content.component';
-import { MenuContentComponent } from './components/menu-content/menu-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainContentComponent,
     NavContentComponent,
-    MenuContentComponent
   ],
   imports: [
     BrowserModule,
